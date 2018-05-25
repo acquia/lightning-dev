ae5f5aa665ec367610566df4e8989ca23b407cd3
Adam Hoenich <adam.hoenich@acquia.com>
2018-05-25 12:59:11 -0400

    Fixed incorrect replacement in RoboFile::configurePhpUnit().

--- Commands/RoboFile.php
+++ Commands/RoboFile.php
@@ -495,8 +495,8 @@ class RoboFile extends Tasks
 
         if (empty($options['with-deprecations']))
         {
-            $search[] = '<env name="SYMFONY_DEPRECATIONS_HELPER" value="weak"/>';
-            $replace[] = '<env name="SYMFONY_DEPRECATIONS_HELPER" value="disabled"/>';
+            $search[] = '<env name="SYMFONY_DEPRECATIONS_HELPER" value="weak_vendors"/>';
+            $replace[] = '<env name="SYMFONY_DEPRECATIONS_HELPER" value="weak"/>';
         }
 
         return $this->collectionBuilder()
