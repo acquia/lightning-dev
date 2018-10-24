0a1cf07571c80ef998c6be4d22d9bfca4f12e811
bendeguz.csirmaz <bendeguz.csirmaz@cheppers.com>
2018-10-24 09:35:55 +0200

    Add use:stable command

---
+++ Commands/ReleaseHistoryClient.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Acquia\Lightning\Commands;
+
+use GuzzleHttp\Client;
+
+/**
+ * Class to fetch release history information about Drupal projects.
+ */
+final class ReleaseHistoryClient {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  private $httpClient;
+
+  /**
+   * ReleaseHistoryClient constructor.
+   *
+   * @param \GuzzleHttp\Client $http_client
+   *   The HTTP client.
+   */
+  public function __construct(Client $http_client) {
+    $this->httpClient = $http_client;
+  }
+
+  /**
+   * Fetches the latest stable release of a project from Drupal.org.
+   *
+   * @param string $name
+   *   The name of the Drupal project (e.g. 'lightning_core').
+   * @param string $api_version
+   *   The API compatibility version (e.g. '8.x').
+   * @param string $range
+   *   The constraint range (e.g. '3.x-dev').
+   *
+   * @return string
+   *   The latest stable release (e.g. '3.2'), or empty string if not found.
+   */
+  public function getLatestStableRelease($name, $api_version, $range) {
+    $release_history = $this->getReleaseHistory($name, $api_version);
+
+    $prefix = "$api_version-";
+    $prefix_length = strlen($prefix);
+
+    // Remove the '.x-dev' suffix from range if present.
+    $suffix = '.x-dev';
+    $suffix_position = -strlen($suffix);
+
+    if (substr($range, $suffix_position) === $suffix) {
+      $range = substr($range, 0, $suffix_position);
+    }
+
+    // Releases are ordered from newest to oldest.
+    foreach ($release_history->releases->release as $release) {
+      $release_version = (string) $release->version;
+
+      // Remove the API version prefix from the release version if exists.
+      // For example, if the release version is '8.x-3.2', it will be '3.2'.
+      if (strncmp($release_version, $prefix, $prefix_length) === 0) {
+        $release_version = substr($release_version, $prefix_length);
+      }
+
+      if (strncmp($release_version, $range, strlen($range)) === 0) {
+        return $release_version;
+      }
+    }
+
+    return '';
+  }
+
+  /**
+   * Fetches a project's release history from Drupal.org.
+   *
+   * @param string $name
+   *   The name of the Drupal project (e.g. 'lightning_core').
+   * @param string $api_version
+   *   The API compatibility version (e.g. '8.x').
+   *
+   * @return \SimpleXMLElement
+   *   The parsed XML response.
+   */
+  private function getReleaseHistory($name, $api_version) {
+    $uri = "https://updates.drupal.org/release-history/$name/$api_version";
+    $data = $this->httpClient->get($uri)->getBody()->getContents();
+
+    return simplexml_load_string($data);
+  }
+
+}
---
+++ tests/src/Unit/ReleaseHistoryClientTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\Tests\lightning_dev\Unit;
+
+use Acquia\Lightning\Commands\ReleaseHistoryClient;
+use Drupal\Tests\UnitTestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
+/**
+ * Tests the ReleaseHistoryClient service.
+ *
+ * @group lightning_dev
+ */
+class ReleaseHistoryClientTest extends UnitTestCase {
+
+  /**
+   * Tests fetching the latest stable release.
+   *
+   * @param string $name
+   *   The name of the Drupal project.
+   * @param string $api_version
+   *   The API compatibility version.
+   * @param string $range
+   *   The constraint range.
+   * @param string $release_history
+   *   The mock response body.
+   * @param string $expected
+   *   The expected result.
+   *
+   * @dataProvider getLatestStableReleaseProvider
+   */
+  public function testGetLatestStableRelease($name, $api_version, $range, $release_history, $expected) {
+    $client = $this->getMockBuilder(Client::class)
+      ->setMethods(['get'])
+      ->getMock();
+    $client->expects($this->once())
+      ->method('get')
+      ->with("https://updates.drupal.org/release-history/$name/$api_version")
+      ->will($this->returnValue(new Response(200, [], $release_history)));
+    $result = (new ReleaseHistoryClient($client))->getLatestStableRelease($name, $api_version, $range);
+    $this->assertSame($expected, $result);
+  }
+
+  /**
+   * Data provider for ::testGetLatestStableRelease().
+   *
+   * @return array
+   *   The test data.
+   */
+  public function getLatestStableReleaseProvider() {
+    $drupal_release_history = '
+      <project>
+        <releases>
+          <release><version>8.6.2</version></release>
+          <release><version>8.6.1</version></release>
+          <release><version>8.5.8</version></release>
+          <release><version>8.5.7</version></release>
+        </releases>
+      </project>
+    ';
+    $lightning_core_release_history = '
+      <project>
+        <releases>
+          <release><version>8.x-3.2</version></release>
+          <release><version>8.x-3.1</version></release>
+          <release><version>8.x-2.11</version></release>
+          <release><version>8.x-2.10</version></release>
+        </releases>
+      </project>
+    ';
+
+    return [
+      'drupal latest' => [
+        'name' => 'drupal',
+        'api_version' => '8.x',
+        'range' => '8.6.x-dev',
+        'release_history' => $drupal_release_history,
+        'expected' => '8.6.2',
+      ],
+      'drupal 8.5.x-dev' => [
+        'name' => 'drupal',
+        'api_version' => '8.x',
+        'range' => '8.5.x-dev',
+        'release_history' => $drupal_release_history,
+        'expected' => '8.5.8',
+      ],
+      'lightning_core latest' => [
+        'name' => 'lightning_core',
+        'api_version' => '8.x',
+        'range' => '3.x-dev',
+        'release_history' => $lightning_core_release_history,
+        'expected' => '3.2',
+      ],
+      'lightning_core 2.x-dev' => [
+        'name' => 'lightning_core',
+        'api_version' => '8.x',
+        'range' => '2.x-dev',
+        'release_history' => $lightning_core_release_history,
+        'expected' => '2.11',
+      ],
+    ];
+  }
+
+}
